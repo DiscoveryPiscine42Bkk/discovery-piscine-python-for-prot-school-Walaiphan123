0b67f5243f7c9230706cde8fdea7db2fc6b7cf5d
Walaiphan123 <42953@prot.ac.th>
2025-03-11 09:24:38 +0700

    ex03

---
+++ cell02/ex03/mult.py
@@ -0,0 +1,12 @@
+a = float(input('กรอกค่า a: '))
+b = float(input('กรอกค่า b: '))
+zero = 0
+x = (a*b)
+print(x)
+if x == zero:
+    print("The result is positive and negative.")
+if x > zero:
+    print("The result is positive")
+if x < zero:
+    print("The result is negative")
+
